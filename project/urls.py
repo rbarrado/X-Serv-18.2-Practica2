17e4c400e4ae30097be90d49356e544de4434a89
feer14agui <f14agui@gmail.com>
2017-04-25 23:44:42 +0200

    proyecto

---
+++ project/urls.py
@@ -0,0 +1,22 @@
+"""project URL Configuration
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.8/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import include, url
+from django.contrib import admin
+from practica2 import views
+
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.processBarra, name='mostrar todo'),
+    url(r'^(\d+)$', views.showByID, name='redirect URLlarga si me dan ID/URLcorta'),
+]
